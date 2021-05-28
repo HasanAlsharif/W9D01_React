d72d1220e239858db74ade8432a555ece68f24e9
Hasan Alsharif <hasanxalsharif@gmail.com>
2021-05-28 23:08:40 +0200

    Q2

---
+++ react_todo_list/src/App.js
@@ -0,0 +1,27 @@
+// 1.to install the complete package : npx create-react-app foldername (from jouzaa : clean react , just type (npm i)) and install others , axios , expres ..etc check it from pkg.json under dependenceis
+// 2.turn on React (npm run start)
+// 3.open on chrome (localhost:3000)
+// Best practice name the cpmponnents pascal case (ex.App)
+
+import React from 'react';
+import './App.css';
+
+const App = () => {
+
+  // ex. const name = 'jack' // const age = 30
+
+
+  return (
+    <>
+     
+    <div className="App">
+     <h1>Todo List</h1>
+    </div>
+    </>
+  );
+};
+
+
+
+
+export default App;
